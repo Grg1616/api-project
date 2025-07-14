81ca5b23103e2c4177f10fe5290742f60a5c53b7
greg161616 <imgregtomco@gmail.com>
2025-07-14 14:58:22 +0800

    Add in edit.php

---
+++ edit.php
@@ -0,0 +1,13 @@
+<?php
+require 'db.php';
+
+$id = $_POST['id'];
+$breed = $_POST['breed'];
+$image_url = $_POST['image_url'];
+$fact = $_POST['fact'];
+
+$stmt = $conn->prepare("UPDATE cats SET breed=?, image_url=?, fact=? WHERE id=?");
+$stmt->bind_param("sssi", $breed, $image_url, $fact, $id);
+$stmt->execute();
+echo "Cat updated successfully";
+?>
